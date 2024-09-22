98ae1ee7ab2f62527aef772a806606d212d331dd
Mariana Buhrer <mah_buhrer@hotmail.com>
2024-09-22 13:49:17 -0300

    first commit

---
+++ src/test/java/infnet/edu/petfriend_transport2/PetfriendTransport2ApplicationTests.java
@@ -0,0 +1,13 @@
+package infnet.edu.petfriend_transport2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetfriendTransport2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
